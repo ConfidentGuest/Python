eece96cbdfaf0ecf7ef6b57f4ae0f8ac4c12df8e
Guest <berestov_02@mail.ru>
2021-04-25 17:08:32 +0400

    git commit 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,11 @@
 my_name = "Hehe" #Yeah, comments.
 print ("I'm learning Python now " + my_name) 
-x = 5
-while x > 0 :
-    print(x)
-    x = x-1
-    print (x);
-
- 
-  
+#x = 5
+#while x > 0 :
+  #  x = x-1
+# print (x);
+#print(range(6))
+#b = list(range(1, 10, 2))# From 1 to 10. Step = 2.
+#print(b)
+#for number in range(5):
+   # print(number)
